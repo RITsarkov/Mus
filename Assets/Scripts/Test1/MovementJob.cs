3714fe18ed8f365e06bb45ea1f00c22570c86da8
Roman Tsarkov <roman.tsarkov@plamee.com>
2019-03-28 21:06:08 +0300

    Тестовые скрипты

---
+++ Assets/Scripts/Test1/MovementJob.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Jobs;
+
+namespace Test1
+{
+    public struct MovementJob : IJobParallelForTransform
+    {
+        public float moveSpeed;
+        public float topBound;
+        public float bottomBound;
+        public float deltaTime;
+
+
+        public void Execute(int index, TransformAccess transform)
+        {
+            Vector3 pos = transform.position;
+            pos += moveSpeed * deltaTime * (transform.rotation * new Vector3(0f, 0f, 1f));
+
+            if (pos.z < bottomBound)
+                pos.z = topBound;
+
+            transform.position = pos;
+        }
+    }
+}
---
+++ Assets/Scripts/Test2/GameManager2.cs
@@ -0,0 +1,95 @@
+﻿
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+using UnityEngine.Jobs;
+
+namespace Test2
+{
+//Просто быстрые настройки из редактора Unity
+    public class GameManager : MonoBehaviour
+    {
+
+        public static GameManager GM;
+
+        [Header("Enemy Setting")] 
+        public GameObject enemyPrefab;
+        public float enemySpeed = 1f;
+
+        [Header("Spawn Settings")]
+        public int enemyCount = 1;
+        public int enemyIncrement = 1;
+
+        [Header("Spown borders")]
+        public float topBound = 10;
+        public float bottomBound = -10;
+        public float leftBound = -10;
+        public float rightBound = 10;
+
+//==============================================================================================
+
+//        private NativeArray<Vector3> positionArray;
+//        private NativeArray<Vector3> velocityArray;
+        private TransformAccessArray transformsArray;
+        private MovementJob2 movementJob;
+        private JobHandle moveHandle;
+//        private GameObject[] EnemyArray;
+
+
+        //При закрытии 
+        private void OnDisable()
+        {
+            //Выполнить все работы
+            moveHandle.Complete();
+            //Отчистить все данные
+            transformsArray.Dispose();
+        }
+
+
+        private void Update()
+        {
+            moveHandle.Complete();
+
+            if (Input.GetButtonDown("space"))
+                AddEnemy(enemyPrefab, enemyCount);
+            
+            
+            var velocity = new NativeArray<Vector3>(500, Allocator.Persistent);
+            for (var i = 0; i < velocity.Length; i++)
+                velocity[i] = new Vector3(0, 0, 1);
+
+            movementJob = new MovementJob2()
+            {
+                transformsArray = transformsArray,
+                velocity = velocity,
+                topBound = topBound,
+                bottomBound = bottomBound,
+                deltaTime = Time.deltaTime
+            };
+            //х3 но вроде бы на этом месте все потоки и запускаются
+            moveHandle = movementJob.Schedule(transformsArray.length, 64);
+
+            for (int i = 0; i < transformsArray.length; i++)
+            {
+                Debug.Log(transformsArray[i].position);
+            }
+
+            //JobHandle.ScheduleBatchedJobs();
+        }
+        
+        private void AddEnemy(GameObject enemyPrefab, int amount)
+        {
+            moveHandle.Complete();
+            transformsArray.capacity = transformsArray.length + amount;
+            for (int i = 0; i < amount; i++)
+            {
+                float xVal = Random.Range(leftBound, rightBound);
+                float zVal = Random.Range(0, 10f);
+                Vector3 pos = new Vector3(xVal, 0, zVal + topBound);
+                Quaternion rot = Quaternion.Euler(0f, 180f, 0f);
+                GameObject obj = Instantiate(enemyPrefab, pos, rot);
+                transformsArray.Add(obj.transform);
+            }
+        }
+    }
+}
