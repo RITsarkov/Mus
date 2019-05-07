7bfebf93387403db9d994af64ffe0f0441e9ce0d
riTsarkov <ritsarkov@gmail.com>
2019-05-07 06:00:07 +0300

    note gui

--- Assets/Scripts/Mus/GameManager.cs
+++ Assets/Scripts/Mus/GameManager.cs
@@ -7,17 +7,25 @@ namespace Mus
     public class GameManager : MonoBehaviour
     {
         private NoteMatrix noteMatrix;
+        private GameObject notePanelPref;
         
         public Text debugNoteMatrix;
         public GameObject notePanel;
         public Canvas canvas;
 
+        public GameObject rNore;
+        public GameObject gNore;
+        public GameObject bNore;
+
         void Start()
         {
             noteMatrix = new NoteMatrix();
+            
+            //Грузим панельку
+            notePanelPref = Instantiate(notePanel, canvas.transform, false);
+            
             visualizeNoteMatix();
-            var notePanelPref = Instantiate(notePanel, new Vector3(0, 0, 0), Quaternion.identity);
-            notePanelPref.transform.parent = canvas.transform;
+            visualizeNote();
         }
         
         private void Update()
@@ -26,10 +34,48 @@ namespace Mus
             {
                 noteMatrix.generateRendomNotes();
                 visualizeNoteMatix();
+                visualizeNote();
             }
         }
 
 
+
+        private void visualizeNote()
+        {
+            var e = notePanelPref.transform.GetEnumerator();
+    
+           int[,] notes = noteMatrix.getNoteMatrix();
+            for (int i = 0; i <= notes.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= notes.GetUpperBound(1); j++)
+                {
+                    if(e.MoveNext())
+                        addNote( (Transform) e.Current, notes[i,j]);
+                }
+            }
+        }
+
+        private void addNote(Transform parrentTransform, int noteId)
+        {
+            foreach (Transform child in parrentTransform)
+            {
+                Destroy(child.gameObject);
+            }
+            
+            
+            switch (noteId)
+            {
+                case 0:
+                    Instantiate(rNore, parrentTransform, false);
+                    break;
+                case 1:
+                    Instantiate(gNore, parrentTransform, false);
+                    break;
+                case 2:
+                    Instantiate(bNore, parrentTransform, false);
+                    break;
+            }
+        }
 
         private void visualizeNoteMatix()
         {
